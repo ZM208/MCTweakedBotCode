9f6a8f5e32f954f4c08e6f8526ad7c64c7a9f9ea
ZM208 <m.elliss@talktalk.net>
2025-05-08 21:47:54 +0100

    updated bot script moved miner logic to it own file updated it as well as started on the main file that will handle connections and results

---
+++ Miner.lua
@@ -0,0 +1,76 @@
+local args = {...}
+stringtoboolean={ ["true"]=true, ["false"]=false }
+function MineArea(length, height, width, up, right)
+    length = length - 1
+    for hei = 1, height do 
+        for wid = 1, width do 
+            for len = 1, length do 
+                turtle.dig();
+                turtle.forward()
+            end 
+            if right then 
+                turtle.turnRight()
+                if (wid) ~= width then  
+                    turtle.dig()
+                    turtle.forward()
+                    turtle.turnRight()
+                    right = false
+                end
+            else 
+                turtle.turnLeft()
+                if (wid) ~= width then
+                    turtle.dig()
+                    turtle.forward()
+                    turtle.turnLeft()
+                    right = true
+                end 
+            end 
+        end 
+        
+        if (right) then 
+            turtle.turnRight()
+        else 
+            turtle.turnLeft()
+        end 
+        if hei ~= height then 
+        print(hei)
+            if (up) then 
+                turtle.digUp()
+                turtle.up()
+            else 
+                turtle.digDown()
+                turtle.down()
+            --right = false
+            end 
+        end 
+    end 
+    if (height % 2) == 1 then 
+        moveForward(length)
+    end  
+    if right then 
+        turtle.turnRight()
+    else 
+        turtle.turnLeft()
+    end 
+    if (height % 2) == 1 then
+        moveForward(width - 1)
+    end 
+    for move = 1, height - 1 do 
+        if up then 
+            turtle.down()
+        else 
+            turtle.up()
+        end 
+    end 
+end
+function moveForward(forwardAmount)
+    for move = 1, forwardAmount do 
+        turtle.forward()
+    end
+end
+if #args < 5 then 
+    print("please enter the following args: length, width, height, up?, right?")
+else    
+    print(args[1])
+    MineArea(tonumber(args[1]), tonumber(args[2]), tonumber(args[3]), stringtoboolean[args[4]], stringtoboolean[args[5]])
+end 
--- main.lua
+++ main.lua
@@ -1,30 +1,23 @@
-local positionlist = {}
+local miner = require("Miner");
+local os = require("os")
+local http = require("http")
 
-MineArea(length, height, width)
-
-function MineArea(length, height, width, up, right)
-    for hei = 0, hei < height do 
-        for wid = 0, wid < width do 
-            for len = 0, len < length do 
-                turtle.dig();
-                turtle.forward()
-            end 
-            if right then 
-                turtle.turnright()
-                turtle.dig()
-                turtle.forward()
-                turtle.turnright()
-                right = false
-            else 
-                turtle.turnleft()
-                turtle.dig()
-                turtle.forward()
-                turtle.turnleft()
-                right = true
-            end 
+function Main()
+    while true do 
+        if (!ws) then
+            AttemptConnection("")
         end 
-        turtle.digUp()
-        turtle.turnleft()
-        turtle.down()
+        ws.recieve()
     end 
-end
+end 
+
+
+function AttemptConnection(connectionUrl)
+    while true do 
+        local ws, wsError = http.websocket(connectionUrl);
+        if (ws and !wsError) then 
+            return ws;
+        end 
+        os.time().sleep(60);
+    end 
+end 
