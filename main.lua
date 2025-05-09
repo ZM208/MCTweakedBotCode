7e9064ca3c5b3aac2e48b38cef085f7455bb64a5
Mason Elliss <m.elliss@talktalk.net>
2025-05-09 23:42:15 +0100

    made some changes to how the main script works now takes a name in args as well as handling errors and messages

--- main.lua
+++ main.lua
@@ -1,23 +1,44 @@
 local miner = require("Miner");
-local os = require("os")
 local http = require("http")
+local connectionUrl = ""; 
+local name = args[1]; 
+local activeWebSocket;
+local wsError; 
 
 function Main()
     while true do 
-        if (!ws) then
-            AttemptConnection("")
+        if (wsError ~= nil) then 
+            wsError = nil; 
+            activeWebSocket = nil
         end 
-        ws.recieve()
+        if (activeWebSocket == nil) then
+            AttemptConnection()
+        end 
+        ResponseHandler(activeWebSocket.recieve())
     end 
 end 
 
 
-function AttemptConnection(connectionUrl)
+function AttemptConnection()
     while true do 
-        local ws, wsError = http.websocket(connectionUrl);
-        if (ws and !wsError) then 
-            return ws;
+        activeWebSocket, wsError = http.websocket(connectionUrl);
+        if (activeWebSocket and !wsError) then 
+            return activeWebSocket;
         end 
         os.time().sleep(60);
     end 
 end 
+function ResponseHandler(message)
+    if (message == "UpdateInfo") then  
+
+    end
+end 
+
+function PrepareMessage()
+
+end 
+
+function DecryptMessage()
+
+end 
+Main()
