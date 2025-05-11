1fe7b302b241ed28af641a9b543cb7c466af7b5e
ZM208 <m.elliss@talktalk.net>
2025-05-11 21:10:37 +0100

    console logging for testing. Testing for bot complete going to launch application and see if it connects properly.

--- main.lua
+++ main.lua
@@ -1,18 +1,21 @@
-local miner = require("Miner");
-local http = require("http")
+require("miner");
 local connectionUrl = ""; 
+local args = {...}
 local name = args[1]; 
 local job = args[2];
 local activeWebSocket;
-local textutils = require("textutils")
 local wsError; 
+
 function Main()
     while true do 
+      print("checking websocket connection")
         if (wsError ~= nil) then 
+          print("error detected: " + wsError)
             wsError = nil; 
             activeWebSocket = nil
         end 
         if (activeWebSocket == nil) then
+            print("websocket not detected starting connection attempt.")
             AttemptConnection()
         end 
         ResponseHandler(activeWebSocket.recieve())
@@ -22,33 +25,43 @@ end
 
 function AttemptConnection()
     while true do 
+        print("attempting to connect to websocket")
         activeWebSocket, wsError = http.websocket(connectionUrl);
-        if (activeWebSocket and !wsError) then 
+        if (activeWebSocket and not wsError) then 
             return activeWebSocket;
         end 
-        os.time().sleep(60);
+        print("failed to connect. Trying again in 60 secs.")
+        os.sleep(60);
     end 
 end 
 function ResponseHandler(message)
+    print("recieved message from server")
     local args = DecryptMessage(message);
     if (args["target"] == "UpdateInfo") then  
+        print("UpdateInfo detected sending update response")
         local message = {}
         message["target"] = "UpdateInfo";
         message["name"] = name;
         message["Job"] = job
         message["currentlyWorking"] = false;
         PrepareMessageAndSend(message)
+        print("sent update response")
     end
     if (args["target"] == "MineArea") then 
+        print("minearea detected executing minearea")
         MineArea(args["length"],args["height"],args["width"],args["goUp"],args["goRight"])
     end
 end 
 
 function PrepareMessageAndSend(args)
-     activeWebSocket.send(textutils.serialiseJSON(args))
+    print("preparing message.")
+    activeWebSocket.send(textutils.serialiseJSON(args))
+    print("sent message")
 end 
 
 function DecryptMessage(message)
+    print("decrypting response message.")
     return textutils.DecryptMessage(message)
 end 
+
 Main()
