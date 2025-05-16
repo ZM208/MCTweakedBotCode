e2ab56c5a17c154573c235d802d67aa3ce2c3a7e
Mason Elliss <m.elliss@talktalk.net>
2025-05-16 22:44:00 +0100

    improvements to bot fully working

--- main.lua
+++ main.lua
@@ -1,46 +1,59 @@
-local miner = require("Miner");
-local http = require("http")
-local connectionUrl = ""; 
+require("miner");
+local connectionUrl = ""; -- your websocket server url here
+local args = {...}
 local name = args[1]; 
 local job = args[2];
-local activeWebSocket;
-local textutils = require("textutils")
-local wsError; 
+activeWebSocket = nil;
+wsError = nil ; 
 function Main()
     while true do 
+      print("checking websocket connection")
         if (wsError ~= nil) then 
+          print("error detected: " + wsError)
             wsError = nil; 
             activeWebSocket = nil
         end 
         if (activeWebSocket == nil) then
+            print("websocket not detected starting connection attempt.")
             AttemptConnection()
-        end 
-        ResponseHandler(activeWebSocket.recieve())
+        end
+        if not pcall(WaitForMessage) then 
+            activeWebSocket = nil
+            end
+    end  
+end 
+ 
+function WaitForMessage()
+    local message = activeWebSocket.receive()
+    if (message ~= nil) then
+        ResponseHandler(message)
     end 
 end 
-
-
 function AttemptConnection()
     while true do 
+        print("attempting to connect to websocket")
         activeWebSocket, wsError = http.websocket(connectionUrl);
-        if (activeWebSocket and !wsError) then 
+        if (activeWebSocket and not wsError) then 
             return activeWebSocket;
         end 
-        os.time().sleep(60);
+        print("failed to connect. Trying again in 60 secs.")
+        Sleep(60);
     end 
 end 
 function ResponseHandler(message)
-    local args = DecryptMessage(message);
-    if (args["target"] == "UpdateInfo") then  
+print(message)
+     args = DecryptMessage(message);
+    print(args)
+    if (args["Target"] == "UpdateInfo") then  
         local message = {}
-        message["target"] = "UpdateInfo";
-        message["name"] = name;
+        message["Target"] = "UpdateInfo";
+        message["Name"] = name;
         message["Job"] = job
-        message["currentlyWorking"] = false;
+        message["CurrentlyWorking"] = false;
         PrepareMessageAndSend(message)
     end
-    if (args["target"] == "MineArea") then 
-        MineArea(args["length"],args["height"],args["width"],args["goUp"],args["goRight"])
+    if (args["Command"] == "MineArea") then 
+        MineArea(args["Length"],args["Height"],args["Width"],args["GoUp"],args["GoRight"])
     end
 end 
 
@@ -49,6 +62,9 @@ function PrepareMessageAndSend(args)
 end 
 
 function DecryptMessage(message)
-    return textutils.DecryptMessage(message)
+    return  textutils.unserialiseJSON(message)
 end 
-Main()
+function Sleep(seconds)
+    os.sleep(seconds)
+end 
+Main() 
